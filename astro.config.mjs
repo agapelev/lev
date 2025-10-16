8b21e5cce27896ba7de7fbc84f7ef17a5865a796
Lev Dessyatykh <dessyatykh@gmail.com>
2025-10-16 12:44:18 +0500

    Batton & Theme Dark

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,19 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
+// ❌ Удалить лишний import image внутри объекта
+// ✅ Если хочешь использовать @astrojs/image, импортируй его здесь:
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  theme: 'public/css/theme.css',
+  integrations: [mdx(), sitemap(), image()],
+                            adapter: cloudflare({
+                              platformProxy: {
+                                enabled: true,
+                              },
+                            }),
 });
